4fd760bb26db6f4f6edc6104d9823125d94ed119
MariyaN23 <sellaite505@gmail.com>
2023-07-17 19:40:30 +0300

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import img from "../src/Rectangle.svg"
+import {Image} from "./components/Image"
+import {Headline} from "./components/Headline";
+import {Text} from "./components/Text";
+import {StyledBtn} from "./components/StyledBtn";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Box>
+            <Image src={img} className="Img" alt="desert"></Image>
+            <Headline>Headline</Headline>
+            <Text>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</Text>
+            <StyledBtn color={"white"} background={"#4E71FE"}>See more</StyledBtn>
+            <StyledBtn color={"#4E71FE"} background={"white"}>Save</StyledBtn>
+        </Box>
     </div>
   );
 }
 
 export default App;
+
+const Box = styled.div`
+  width: 300px;
+  height: 350px;
+  border-radius: 15px;
+  background: #FFF;
+  box-shadow: 0px 4px 20px 5px rgba(0, 0, 0, 0.10);
+  margin-left: auto;
+  margin-right: auto;
+`
